2a72cea1a26ea7ad0eed444e8f963af4dfb35905
Luis <luisbaut93@gmail.com>
2026-05-14 21:18:29 -0600

    feat(order): Add OrderStatusHistory domain class

--- src/main/java/com/bautista/order_flow_service/order/domain/OrderStatusHistory.java
+++ src/main/java/com/bautista/order_flow_service/order/domain/OrderStatusHistory.java
@@ -1,4 +1,26 @@
 package com.bautista.order_flow_service.order.domain;
 
+import com.github.f4b6a3.uuid.UuidCreator;
+
+import java.time.Instant;
+import java.util.UUID;
+
 public class OrderStatusHistory {
+    private UUID historyId;
+    private UUID orderId;
+    private String oldStatus;
+    private String newStatus;
+    private String reason;
+    private String eventId;
+    private Instant changedAt;
+
+    public OrderStatusHistory (UUID orderId, String oldStatus, String newStatus, String reason, String eventId, Instant changedAt) {
+        this.historyId = UuidCreator.getTimeOrdered();
+        this.orderId = orderId;
+        this.oldStatus = oldStatus;
+        this.newStatus = newStatus;
+        this.reason = reason;
+        this.eventId = eventId;
+        this.changedAt = changedAt;
+    }
 }
